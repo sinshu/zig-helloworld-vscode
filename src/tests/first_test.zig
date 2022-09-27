6ecef3424e0f53bc5644653ff54505a64db24ae4
田中信秋 <sinshu.d@gmail.com>
2022-09-28 00:15:12 +0900

    Update.

--- src/tests/first_test.zig
+++ src/tests/first_test.zig
@@ -2,8 +2,7 @@ const std = @import("std");
 
 test "first test"
 {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var s = "hello";
+    var sub = s[0..4];
+    std.debug.assert(std.mem.eql(u8, sub, "hell"));
 }
